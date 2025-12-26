6fbd98eee3264afb1941afe41fc2c7e95e899abd
Jason Law <jason.e.law@gmail.com>
2025-12-26 01:39:22 -0800

    Add a new entry and set up new development environment

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 export default {
-	title: "Eleventy Base Blog v9",
-	url: "https://example.com/",
+	title: "Jason Law",
+	url: "https://wiretail.org/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Just my home on the net.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Jason Law",
+		email: "",
+		url: "https://wiretail.org/about-me/"
 	}
 }
